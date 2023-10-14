c9323d19e98381f6d8ec0dc7ce40ace74d831294
hansel.tjahyadi <hansel.tjahyadi@shopee.com>
2023-10-14 15:11:38 +0700

    feat: user edit profile complete minus referral code. Currently working on redesigning the user navbar

--- src/components/UserNav/index.tsx
+++ src/components/UserNav/index.tsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useStoreLoginPersist } from "@/store/store";
 import { useRouter } from "next/router";
+import Image from "next/image";
+import styles from "./UserNav.module.css";
 
 interface HomeNavProps {
   currentPage?: string;
+  picture?: string;
+}
+
+interface IAdmin {
+  id: number;
+  email: string;
+  fullname: string;
+  password: string;
+  phone: string;
+  photo: string;
+  role: string;
 }
 
 function UserNav(props: HomeNavProps) {
   const stateLoginPersist = useStoreLoginPersist();
   const router = useRouter();
+
+  const [adminData, setAdminData] = useState<IAdmin>();
+  const [imageFile, setImageFile] = useState<string>("");
+
+  const redirectEditProfile = () => {
+    router.push("/user/editProfile");
+  };
 
   const redirectTransfer = () => {
     alert();
@@ -32,21 +52,68 @@ function UserNav(props: HomeNavProps) {
     alert();
   };
 
+  const urlToLink = async (link: string) => {
+    const randomName =
+      Math.floor(Math.random() * (999999 - 100000) + 100000).toString() +
+      ".jpg";
+
+    let imgFile = fetch(link).then(async (response) => {
+      const blob = await response.blob();
+      const file = new File([blob], randomName);
+      return file;
+    });
+
+    return imgFile;
+  };
+
+  const getFile = async (link: File | string) => {
+    let result = await urlToLink(typeof link === "string" ? link : "");
+    return result;
+  };
+
+  const getAdminData = async () => {
+    if (props.picture) {
+      let profileImageFile = await getFile(props.picture);
+      setImageFile(URL.createObjectURL(profileImageFile));
+    }
+
+    try {
+      const response = await fetch(
+        `http://localhost:2000/users/${stateLoginPersist.id}`
+      );
+      const result = await response.json();
+      setAdminData(result);
+
+      if (result.photo !== "") {
+        let profileImageFile = await getFile(result.photo);
+        setImageFile(URL.createObjectURL(profileImageFile));
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getAdminData();
+  }, [props.picture]);
+
   return (
     <div className="user-nav-div ">
-      <div className="flex user-nav-wrapper mx-[200px] py-[18px] justify-between">
+      <div
+        className={`${styles.navArea} flex user-nav-wrapper mx-[200px] py-[18px] justify-between`}
+      >
         <div className="logo-section flex items-center">
           <h1 className="text-[24px] font-bold">Courier</h1>
         </div>
-        <div className="nav-selection">
-          <ul className="flex gap-10 items-center pt-[8px] font-semibold text-sm mr-[110px]">
+        <div className={`${styles.centerSelectionDisplay}`}>
+          <ul className="flex gap-10 items-center pt-[8px] font-semibold text-sm">
             <li
               className={`text-secondarytext ${
                 props.currentPage === "home" ? "underline" : ""
               } hover:cursor-pointer`}
               onClick={redirectHome}
             >
-              Home
+              Shipping
             </li>
             <li
               className={`text-secondarytext ${
@@ -54,7 +121,15 @@ function UserNav(props: HomeNavProps) {
               } hover:cursor-pointer`}
               onClick={redirectTransfer}
             >
-              Transfer
+              Address
+            </li>
+            <li
+              className={`text-secondarytext ${
+                props.currentPage === "transfer" ? "underline" : ""
+              } hover:cursor-pointer`}
+              onClick={redirectTransfer}
+            >
+              Payment
             </li>
             <li
               className={`text-secondarytext ${
@@ -72,12 +147,29 @@ function UserNav(props: HomeNavProps) {
             >
               Games
             </li>
+          </ul>
+        </div>
+        <div
+          className={`user-picture hover:cursor-pointer mr-[110px] ${styles.navRight} `}
+          onClick={redirectEditProfile}
+        >
+          <ul
+            className={`flex gap-5 items-center pt-[-1px] font-semibold text-sm`}
+          >
             <li
-              className="text-secondarytext hover:cursor-pointer"
-              onClick={redirectLogout}
-            >
-              Logout
-            </li>
+              className={`text-secondarytext ${styles.nameDisplay}`}
+            >{`${adminData?.fullname}`}</li>
+            <Image
+              src={`${!imageFile ? "/images/defaultuser.png" : imageFile}`}
+              alt="Nothing"
+              width={25}
+              height={25}
+              className={`w-[45px] h-[45px]`}
+              style={{
+                objectFit: "cover",
+                borderRadius: "100%",
+              }}
+            />
           </ul>
         </div>
       </div>
---
+++ src/pages/user/editProfile/index.tsx
@@ -0,0 +1,254 @@
+import React, { useState, useEffect } from "react";
+import { useStoreLoginPersist } from "@/store/store";
+import { useRouter } from "next/router";
+import Input from "@/components/Input";
+import Image from "next/image";
+import Button from "@/components/Button";
+import styles from "../../../styles/AdminPage.module.css";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import UserNav from "@/components/UserNav";
+
+interface IAdmin {
+  id: number;
+  email: string;
+  fullname: string;
+  password: string;
+  phone: string;
+  photo: string;
+  role: string;
+}
+
+function UserEditProfile() {
+  const [imageFile, setImageFile] = useState<File | null>(null);
+  const [adminData, setAdminData] = useState<IAdmin>({
+    id: 0,
+    email: "",
+    fullname: "",
+    password: "",
+    phone: "",
+    photo: "",
+    role: "",
+  });
+
+  const [showLoading, setShowLoading] = useState<boolean>(false);
+
+  const successMessage = () => toast("Profile update success!");
+
+  const invalidPhoneMessage = () =>
+    toast("Invalid phone format! Start with +62 or 0 for your phone number.");
+
+  const urlToLink = async (link: string) => {
+    const randomName =
+      Math.floor(Math.random() * (999999 - 100000) + 100000).toString() +
+      ".jpg";
+
+    let imgFile = fetch(link).then(async (response) => {
+      const blob = await response.blob();
+      const file = new File([blob], randomName);
+      return file;
+    });
+
+    return imgFile;
+  };
+
+  const getFile = async (link: File | string) => {
+    let result = await urlToLink(typeof link === "string" ? link : "");
+    return result;
+  };
+
+  const stateLoginPersist = useStoreLoginPersist();
+  const router = useRouter();
+
+  const getAdminData = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:2000/users/${stateLoginPersist.id}`
+      );
+      const result = await response.json();
+      setAdminData(result);
+
+      if (result.photo !== "") {
+        let profileImageFile = await getFile(result.photo);
+        setImageFile(profileImageFile);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const profileUpload = async (file: any, folder: string) => {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", folder);
+    let data = "";
+
+    let response = await axios.post(
+      `https://api.cloudinary.com/v1_1/dsngblge5/image/upload`,
+      formData
+    );
+
+    data = response.data.url;
+    return data;
+  };
+
+  const validPhoneNumber = (phone: string) => {
+    return /^(^\+62|62|^08)(\d{3,4}-?){2}\d{3,4}$/.test(phone);
+  };
+
+  const submit = async (e: any) => {
+    e.preventDefault();
+
+    if (!validPhoneNumber(adminData.phone)) {
+      invalidPhoneMessage();
+      return;
+    }
+
+    setShowLoading(true);
+
+    if (imageFile) {
+      let profilePictureLink = await profileUpload(
+        imageFile,
+        "userProfileImage"
+      );
+
+      let currentData = adminData;
+      currentData.photo = profilePictureLink;
+      setAdminData(currentData);
+    }
+
+    const submitData = adminData;
+
+    axios
+      .patch(`http://localhost:2000/users/${stateLoginPersist.id}`, submitData)
+      .then(() => {
+        setShowLoading(false);
+        successMessage();
+      });
+  };
+
+  useEffect(() => {
+    if (stateLoginPersist.id === 0 || stateLoginPersist.isAdmin) {
+      router.push("/error");
+    }
+
+    getAdminData();
+  }, []);
+
+  return (
+    <div className="admin-profile-div min-h-screen bg-slate-50">
+      <UserNav />
+      <div
+        className={`${styles.adminMainArea} admin-profile-content mx-[200px] py-[18px] pt-[50px]`}
+      >
+        <ToastContainer />
+        <div className="titles-section">
+          <h1 className="text-[30px] text-center font-medium">Edit profile</h1>
+        </div>
+      </div>
+      <div
+        className={`${styles.adminMainArea} form-area mx-[350px] py-[18px] pt-[20px]`}
+      >
+        <form action="" onSubmit={submit}>
+          <div className={`${styles.formArea} flex justify-around`}>
+            <div className="input-form-area">
+              <Input
+                label="Email"
+                type="email"
+                name="adminEditEmail"
+                styling="pb-5"
+                width="w-[300px]"
+                required={true}
+                value={adminData?.email}
+                onChange={(e) => {
+                  setAdminData({
+                    ...adminData,
+                    email: e.target.value,
+                  });
+                }}
+              />
+              <Input
+                label="Full Name"
+                type="text"
+                name="adminEditFullName"
+                styling="pb-5"
+                width="w-[300px]"
+                required={true}
+                value={adminData?.fullname}
+                onChange={(e) => {
+                  setAdminData({
+                    ...adminData,
+                    fullname: e.target.value,
+                  });
+                }}
+              />
+              <Input
+                label="Phone Number"
+                type="string"
+                name="adminEditPhoneNumber"
+                styling="pb-5"
+                width="w-[300px]"
+                required={true}
+                value={adminData?.phone}
+                onChange={(e) => {
+                  setAdminData({
+                    ...adminData,
+                    phone: e.target.value,
+                  });
+                }}
+              />
+            </div>
+            <div
+              className={`${styles.formImageArea} flex-col justify-center items-center admin-edit-photo`}
+            >
+              <Image
+                src={`${
+                  imageFile === null
+                    ? "/images/defaultuser.png"
+                    : URL.createObjectURL(imageFile)
+                }`}
+                alt="Nothing"
+                width={200}
+                height={200}
+                className={`${styles.imgArea} w-[200px] h-[200px]`}
+                style={{
+                  objectFit: "fill",
+                  borderRadius: "100%",
+                }}
+              />
+              <br />
+              <label className="custom-file-upload bg-amber-400 hover:cursor-pointer hover:bg-amber-500 p-4 rounded-[10px] ml-1.5 text-center">
+                <input
+                  type="file"
+                  className="hidden"
+                  onChange={(e) => {
+                    if (e.target.files !== null) {
+                      setImageFile(e.target.files[0]);
+                    }
+                  }}
+                />
+                {!adminData.photo
+                  ? "Upload profile photo"
+                  : "Replace profile photo"}
+              </label>
+            </div>
+          </div>
+          <div className="flex submit-btn justify-center pt-[100px]">
+            {showLoading ? (
+              <AiOutlineLoading3Quarters className="animate-spin text-3xl" />
+            ) : (
+              <Button
+                text="Save Changes"
+                styling="p-4 mb-[50px] bg-amber-400 rounded-[10px] w-[200px] hover:bg-amber-500"
+              />
+            )}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default UserEditProfile;
