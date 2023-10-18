83b4a5a99eb39bd9075511b834b4ee076ca3149c
hansel.tjahyadi <hansel.tjahyadi@shopee.com>
2023-10-18 16:39:50 +0700

    feat(user): manage address pagination complete, will still need refinement for color

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styles from "./Pagination.module.css";
+
+interface PaginationProps {
+  page: number;
+  count: number;
+  size: number;
+  movePage: (page: number) => void;
+}
+
+function Pagination(props: PaginationProps) {
+  const firstDisabled = props.page === 1;
+  const nextDisabled = props.page === Math.ceil(props.count / props.size);
+
+  const showButton =
+    Math.ceil(props.count / props.size) === 1
+      ? [1]
+      : Math.ceil(props.count / props.size) === 2
+      ? [1, 2]
+      : Math.ceil(props.count / props.size) === 3 || props.page === 1
+      ? [1, 2, 3]
+      : props.page === Math.ceil(props.count / props.size)
+      ? [props.page - 2, props.page - 1, props.page]
+      : [props.page - 1, props.page, props.page + 1];
+
+  return (
+    <div className="pagination-div flex">
+      <button
+        className={`${styles.firstBtn} text-primary ${
+          firstDisabled ? "bg-[#b4b4b4]" : ""
+        }`}
+        onClick={() => props.movePage(1)}
+        disabled={firstDisabled}
+      >
+        First
+      </button>
+      {showButton.map((pageNum) => (
+        <button
+          key={pageNum}
+          onClick={() => props.movePage(pageNum)}
+          className={`${styles.pageBtn} ${
+            pageNum === props.page
+              ? "bg-amber-400 text-orange-600"
+              : "bg-white text-primary"
+          }`}
+        >
+          {pageNum}
+        </button>
+      ))}
+      <button
+        className={`${styles.nextBtn} text-primary ${
+          nextDisabled ? "bg-[#b4b4b4]" : ""
+        }`}
+        onClick={() => props.movePage(props.page + 1)}
+        disabled={nextDisabled}
+      >
+        Next
+      </button>
+    </div>
+  );
+}
+
+export default Pagination;
--- src/pages/user/address/manage/index.tsx
+++ src/pages/user/address/manage/index.tsx
@@ -1,38 +1,88 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import UserNav from "@/components/UserNav";
 import UserHeader from "@/components/HeaderSection";
 import TableHead from "@/components/TableHead";
 import TableData from "@/components/TableData";
+import Pagination from "@/components/Pagination";
+import { useStoreLoginPersist } from "@/store/store";
+import { BASE_URL } from "@/constants/constants";
+import { IAddress } from "@/types/types";
 
 function ManageAddress() {
+  const stateLoginPersist = useStoreLoginPersist();
+  const [addressData, setAddressData] = useState<IAddress[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [count, setCount] = useState<number>(0);
+  const [size, setSize] = useState<number>(0);
+
+  const movePage = (pageNum: number) => {
+    setCurrentPage(pageNum);
+  };
+
+  const getAddressData = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/users/${stateLoginPersist.id}`);
+      const result = await response.json();
+
+      const addressResponse = await fetch(
+        `${BASE_URL}/userAddress/${result.addressId}`
+      );
+      const addressResult = await addressResponse.json();
+      setSize(addressResult.addressList.length);
+
+      setAddressData(
+        addressResult.addressList.slice((currentPage - 1) * 5, currentPage * 5)
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getAddressData();
+  }, [currentPage]);
+
   return (
     <div>
       <UserNav currentPage="address" />
       <div className="header-section pb-8">
         <UserHeader title="Manage Address" />
       </div>
-      <div className="table-section mx-[350px] mobile:mx-auto mobile:overflow-scroll">
-        <table className="table-area w-full">
-          <tbody>
-            <TableHead />
-            <TableData
-              index={1}
-              id={1}
-              address="Jalan Mega Kuningan Barat III Lot 10.1-6, RT.3/RW.3"
-              city="Jakarta Selatan"
-              province="DKI Jakarta"
-              zip="12230"
-            />
-            <TableData
-              index={2}
-              id={2}
-              address="Jalan Mega Kuningan Barat III Lot 10.1-6, RT.3/RW.3"
-              city="Jakarta Selatan"
-              province="DKI Jakarta"
-              zip="12230"
-            />
-          </tbody>
-        </table>
+      <div className="content-wrapper">
+        {addressData.length === 0 ? (
+          <h1 className="text-center font-medium text-[25px]">
+            Nothing to see here! Go and add a new address!
+          </h1>
+        ) : (
+          <div className="content">
+            <div className="table-section mx-[350px] mobile:mx-auto mobile:overflow-scroll">
+              <table className="table-area w-full">
+                <tbody>
+                  <TableHead />
+                  {addressData.map((data, index) => (
+                    <TableData
+                      key={index}
+                      index={index + 1}
+                      id={data.id}
+                      address={data.address}
+                      city={data.city}
+                      province={data.province}
+                      zip={data.zip}
+                    />
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            <div className="flex justify-center pagination-section mx-[350px] pt-[100px] pb-[50px] mobile:mx-auto">
+              <Pagination
+                page={currentPage}
+                count={size}
+                size={5}
+                movePage={movePage}
+              />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
