1eb5e7c84cab0013679f2007dcfd200a42e2b0cc
hansel.tjahyadi <hansel.tjahyadi@shopee.com>
2023-10-24 19:46:43 +0700

    feat(user): added sorting option to manage shipping

--- src/pages/user/shipping/manage/index.tsx
+++ src/pages/user/shipping/manage/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import UserNav from "@/components/UserNav";
-import UserHeader from "@/components/HeaderSection";
 import Input from "@/components/Input";
 import ShippingTableHead from "@/components/ShippingTableHead";
 import { IShippingData } from "@/types/types";
@@ -8,11 +7,11 @@ import ShippingTableData from "@/components/ShippingTableData";
 import Pagination from "@/components/Pagination";
 import { useStoreLoginPersist } from "@/store/store";
 import { BASE_URL } from "@/constants/constants";
-import provinces from "@/database/provinces.json";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Payment from "@/components/Payment";
 import ReviewModal from "@/components/ReviewModal";
+import Dropdown from "@/components/Dropdown/Dropdown";
 
 function ManageShipping() {
   const stateLoginPersist = useStoreLoginPersist();
@@ -21,6 +20,7 @@ function ManageShipping() {
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [count, setCount] = useState<number>(0);
   const [currentShippingId, setCurrentShippingId] = useState<string>("");
+  const [sortBy, setSortBy] = useState<string>("");
 
   const movePage = (pageNum: number) => {
     setCurrentPage(pageNum);
@@ -56,6 +56,118 @@ function ManageShipping() {
       shippingResult.shippingList = filteredArray;
       setCount(shippingResult.shippingList.length);
 
+      if (sortBy === "Size - ASC") {
+        const sortedArray = shippingResult.shippingList.sort(function (
+          a: IShippingData,
+          b: IShippingData
+        ) {
+          return a.length * a.width * a.height - b.length * b.width * b.height;
+        });
+
+        setShippingData(
+          sortedArray.slice((currentPage - 1) * 5, currentPage * 5)
+        );
+      } else if (sortBy === "Size - DESC") {
+        const sortedArray = shippingResult.shippingList.sort(function (
+          a: IShippingData,
+          b: IShippingData
+        ) {
+          return b.length * b.width * b.height - a.length * a.width * a.height;
+        });
+
+        setShippingData(
+          sortedArray.slice((currentPage - 1) * 5, currentPage * 5)
+        );
+      } else if (sortBy === "Weight - ASC") {
+        const sortedArray = shippingResult.shippingList.sort(function (
+          a: IShippingData,
+          b: IShippingData
+        ) {
+          return a.weight - b.weight;
+        });
+
+        setShippingData(
+          sortedArray.slice((currentPage - 1) * 5, currentPage * 5)
+        );
+      } else if (sortBy === "Weight - DESC") {
+        const sortedArray = shippingResult.shippingList.sort(function (
+          a: IShippingData,
+          b: IShippingData
+        ) {
+          return b.weight - a.weight;
+        });
+
+        setShippingData(
+          sortedArray.slice((currentPage - 1) * 5, currentPage * 5)
+        );
+      } else if (sortBy === "Category - ASC") {
+        const priority = [
+          "Food",
+          "Medical Supplies",
+          "Others",
+          "Personal Items",
+        ];
+        const sortedArray = shippingResult.shippingList.sort(function (
+          a: IShippingData,
+          b: IShippingData
+        ) {
+          return priority.indexOf(a.category) - priority.indexOf(b.category);
+        });
+
+        setShippingData(
+          sortedArray.slice((currentPage - 1) * 5, currentPage * 5)
+        );
+      } else if (sortBy === "Category - DESC") {
+        const priority = [
+          "Food",
+          "Medical Supplies",
+          "Others",
+          "Personal Items",
+        ];
+        const sortedArray = shippingResult.shippingList.sort(function (
+          a: IShippingData,
+          b: IShippingData
+        ) {
+          return priority.indexOf(b.category) - priority.indexOf(a.category);
+        });
+
+        setShippingData(
+          sortedArray.slice((currentPage - 1) * 5, currentPage * 5)
+        );
+      } else if (sortBy === "Payment - Sort by paid") {
+        const priority = [true, false];
+        const sortedArray = shippingResult.shippingList.sort(function (
+          a: IShippingData,
+          b: IShippingData
+        ) {
+          return (
+            priority.indexOf(a.alreadyPaid) - priority.indexOf(b.alreadyPaid)
+          );
+        });
+
+        setShippingData(
+          sortedArray.slice((currentPage - 1) * 5, currentPage * 5)
+        );
+      } else if (sortBy === "Payment - Sort by unpaid") {
+        const priority = [false, true];
+        const sortedArray = shippingResult.shippingList.sort(function (
+          a: IShippingData,
+          b: IShippingData
+        ) {
+          return (
+            priority.indexOf(a.alreadyPaid) - priority.indexOf(b.alreadyPaid)
+          );
+        });
+
+        setShippingData(
+          sortedArray.slice((currentPage - 1) * 5, currentPage * 5)
+        );
+      } else {
+        setShippingData(
+          filteredArray.slice((currentPage - 1) * 5, currentPage * 5)
+        );
+      }
+
       setShippingData(
         shippingResult.shippingList.slice(
           (currentPage - 1) * 5,
@@ -69,11 +181,7 @@ function ManageShipping() {
 
   useEffect(() => {
     getShippingData();
-  }, [currentPage, search]);
-
-  const getProvince = (id: string) => {
-    return provinces.provinces[parseInt(id) - 1].province;
-  };
+  }, [currentPage, search, sortBy]);
 
   const [showPayment, setShowPayment] = useState<boolean>(false);
   const paidMessage = () =>
@@ -121,21 +229,42 @@ function ManageShipping() {
       <div>
         <UserNav currentPage="shipping" />
         <ToastContainer />
-        <div className="header-section flex items-center pb-8 mobile:flex-col">
-          <UserHeader
-            title="Manage Shipping"
-            description="To pay for a shipping, click on the word 'UNPAID'. Otherwise you're good!"
+        <div className="flex justify-between items-center view-earnings-header mx-[350px] pt-[50px] pb-[30px] mobile:flex-col mobile:mx-auto mobile: gap-6">
+          <h1 className="text-[30px] font-medium mt-2 text-amber-600">
+            Manage Shipping
+          </h1>
+          <Dropdown
+            label="Sort by"
+            flexLabel="flex items-center gap-6 mt-3"
+            labelStyle="font-bold pb-2 pt-2"
+            width="w-[300px] mobile:w-[200px]"
+            onChange={(e) => setSortBy(e)}
+            options={[
+              "None",
+              "Size - ASC",
+              "Size - DESC",
+              "Weight - ASC",
+              "Weight - DESC",
+              "Category - ASC",
+              "Category - DESC",
+              "Payment - Sort by paid",
+              "Payment - Sort by unpaid",
+            ]}
           />
           <Input
             label=""
             type="text"
             name="search"
-            styling="mt-8 ml-[28px] mobile:mx-auto"
+            styling=""
             width="w-[300px]"
             placeholder="Search shipping description here"
             onChange={(e) => setSearch(e.target.value)}
           />
         </div>
+        <h1 className="text-center pb-8">
+          To pay for a shipping, click on the word &apos;UNPAID&apos;. Otherwise
+          you&apos;re good!
+        </h1>
         <div className="content-wrapper">
           {shippingData.length === 0 ? (
             <h1 className="text-center font-medium text-[25px]">
